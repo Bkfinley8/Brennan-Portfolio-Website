1f243f67fd2a19e5d2dc22dc53bd3dad87a18e53
Bkfinley8 <brennan8192@gmail.com>
2025-01-16 02:10:00 -0500

    Update next js version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,8 +8,10 @@ import { Container, Div1, Div2, Div3, NavLink, SocialIcons, Span } from './Heade
 const Header = () =>  (
   <Container>
     <Div1>
-      <Link href="/" style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px' }}>
+      <Link href="/">
+        <a style={{ display: 'flex', alignItems: 'center', color: 'white', marginBottom: '20px' }}>
           <DiCssdeck size="3rem" /> <Span>Portfolio</Span>
+        </a>
       </Link>
     </Div1>
     <Div2>
